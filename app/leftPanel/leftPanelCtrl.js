54d20d9d8c4b4a63a7bb4cc5ddb705d0565c615c
Yury <coddev@gmail.com>
2015-12-05 15:04:49 +0300

    fix button logic

--- app/leftPanel/leftPanelCtrl.js
+++ app/leftPanel/leftPanelCtrl.js
@@ -15,6 +15,36 @@
             angular.element(".panel-body-info").css("display", "none");
         };
 
+        //обработчик клик по знаку
+        /************************************************************************************************************/
+        //mode === "mode2" -- режим работы картинок для вкладок добавление
+        $scope.imgClick = function(){
+            this.mleave = true;
+
+            //убираем задаем серый фон для знако в
+            //установка состояние выбран //removeClass
+            if(this.sign.checked){
+                this.sign.checked = false;
+            }
+            //установка состояния не выбран //addClass
+            else{
+                if(this.addPanelImg){//логика работы для вкладке addPanel
+                    $scope.data.modelSigns.addPanel.signs.forEach(function(x){
+                        x.checked = false;
+                    });
+                }
+                this.sign.checked = true;
+            }
+        }
+
+        $scope.onMouseLeave = function(){
+            if(this.sign.checked){
+                this.mleave = true;
+            }else{
+                this.mleave = false;
+            }
+        };
+        /************************************************************************************************************/
         //leafletService.addMarkers();
     }
 
